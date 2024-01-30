f660e2a4250d726a311f129dc7a4f076fb3602b4
Abdullah <143931418+abdullah001111@users.noreply.github.com>
2024-01-30 08:14:00 +0600

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ class Config(object):
     TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
     
     # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
+    TECH_VJ = bool(environ.get('TECH_VJ', False)) # Set False If you want shortlink off else True
     TECH_VJ_URL = environ.get('TECH_VJ_URL', 'publicearn.com') # your shortlink url domain or url without https://
     TECH_VJ_API = environ.get('TECH_VJ_API', 'b11f986901b8c56f7c4b8ecbafaed2afc1749fef') # your url shortner api
     TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/Mayhem_Support")
